37788316c471774dcf3de0994a484984e4e133fc
vule2303 <leductuanvu5@gmail.com>
2023-04-19 20:06:18 +0700

    commit

--- Domain_WebAPI/Model/DTO/AddBookRequestDTO.cs
+++ Domain_WebAPI/Model/DTO/AddBookRequestDTO.cs
@@ -2,5 +2,16 @@
 {
     public class AddBookRequestDTO
     {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool IsReal { get; set; }
+        public DateTime? DateRead { get; set; }
+        public int? Rate { get; set; }
+        public string? Genre { get; set; }
+        public string? CoverUrl { get; set; }
+        public DateTime DateAdded { get; set; }
+        //navigation Properties-
+        public int PublisherID { get; set; }
+        public List<int> AuthorIds { get;set; }
     }
 }
--- Domain_WebAPI/Model/DTO/BookDTO.cs
+++ Domain_WebAPI/Model/DTO/BookDTO.cs
@@ -2,5 +2,16 @@
 {
     public class BookDTO
     {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime? DateRead { get; set; }
+        public int? Rate { get; set; }
+        public string? Genre { get; set; }
+        public string? CoverUrl { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string PublisherName { get; set; }
+        public List<string> AuthorNames { get; set; }
     }
 }
